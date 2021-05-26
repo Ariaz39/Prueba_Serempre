4a72f4caaa12cb3d54f137162e658f67ee35e2ce
Alejo Ariaz <ariaz39@hotmail.com>
2021-05-26 17:53:48 -0500

    Se corrigio el problema de Js con el boton de Logout y el datatable.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
     <!-- Styles -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.24/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
 </head>
 <body>
 <div class="container">
@@ -86,6 +87,7 @@
 </div>
 
 
+<script src="{{asset('js/app.js')}}"></script>
 <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
 <script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.10.24/js/dataTables.bootstrap4.min.js"></script>
